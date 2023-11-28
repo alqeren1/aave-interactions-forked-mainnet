52242286115167c3bb9b9eb6df69bb991d1d8623
alq <alq@mail.com>
2023-11-28 21:42:51 +0300

    last commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -40,6 +40,8 @@ module.exports = {
       { version: "0.8.8" },
       { version: "0.7.0" },
       { version: "0.6.12" },
+      { version: "0.6.0" },
+
       { version: "0.4.18" },
     ],
   },
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,5 +1,6 @@
 const { getNamedAccounts } = require("hardhat");
 const { getWeth, AMOUNT } = require("./GETWETH.JS");
+const { string } = require("hardhat/internal/core/params/argumentTypes");
 
 async function main() {
   await getWeth();
@@ -14,11 +15,36 @@ async function main() {
 
   //deposit
   const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const daiTokenAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
 
   await approve(wethTokenAddress, lendingPool.target, AMOUNT, deployerSigner);
   console.log("Depositing...");
   await lendingPool.deposit(wethTokenAddress, AMOUNT, deployerSigner, 0);
   console.log("Deposited");
+
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+    lendingPool,
+    deployer
+  );
+  const price = await getDaiPrice();
+  console.log("Dai/Eth price: " + price.toString() / 10 ** 18);
+  const amountDaiToBorrow =
+    (availableBorrowsETH.toString() * 0.95 * (1 / price.toString())) / 10 ** 18;
+  console.log("You can borrow " + amountDaiToBorrow + " DAI");
+  const amountDaiToBorrowWei = ethers.parseEther(amountDaiToBorrow.toString());
+  await borrow(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer);
+  const dai = await ethers.getContractAt(
+    "IERC20",
+    daiTokenAddress,
+    deployerSigner
+  );
+  console.log(
+    "Your DAI amount: " + (await dai.balanceOf(deployer)).toString() / 10 ** 18
+  );
+  await getBorrowUserData(lendingPool, deployer);
+
+  await repay(amountDaiToBorrowWei, daiTokenAddress, lendingPool, deployer);
+  await getBorrowUserData(lendingPool, deployer);
 }
 
 async function getLendingPool(account) {
@@ -49,6 +75,44 @@ async function approve(contractAddress, spender, amountToSpend, account) {
   await tx.wait(1);
   console.log("Approved!");
 }
+async function repay(amount, daiAddress, lendingPool, account) {
+  await approve(daiAddress, lendingPool.target, account);
+  const repayTx = await lendingPool.repay(daiAddress, amount, 2, account);
+  await repayTx.wait(1);
+  console.log("Repaid!!");
+}
+
+async function borrow(daiAddress, lendingPool, amountDaiToBorrowWei, account) {
+  const borrowTx = await lendingPool.borrow(
+    daiAddress,
+    amountDaiToBorrowWei,
+    2, // Assuming you want variable interest rate
+    0, // Referral code, 0 if not applicable
+    account
+  );
+  await borrowTx.wait(1);
+  console.log("You've borrowed!");
+}
+async function getBorrowUserData(lendingPool, account) {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await lendingPool.getUserAccountData(account);
+  console.log("Deposited amount: " + totalCollateralETH);
+  console.log("Your debt: " + totalDebtETH);
+  console.log("Available ETH to borrow: " + availableBorrowsETH);
+  return { availableBorrowsETH, totalDebtETH };
+}
+
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    "0x773616E4d11A78F511299002da57A0a94577F1f4"
+  );
+  const price = (await daiEthPriceFeed.latestRoundData())[1];
+  const stringPrice = price.toString() / 10 ** 18;
+  console.log("Eth/Dai price is: " + 1 / stringPrice);
+  return price, stringPrice;
+}
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
